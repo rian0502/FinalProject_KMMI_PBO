d5aea1fdc3a5e15a314419474b11d6b5afdf4119
Muhammad Febrian Hasibuan <83405871+rian0502@users.noreply.github.com>
2021-09-26 19:53:23 +0700

    get

--- FinalProjectPBO/src/finalprojectpbo/Main.java
+++ FinalProjectPBO/src/finalprojectpbo/Main.java
@@ -11,25 +11,18 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-/**
- *
- * @author muham
- */
 public class Main extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("view/view.fxml"));
-        
-        Scene scene = new Scene(root);
-        
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("view/view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 350, 260);
+        stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
     }
 
-    /**
-     * @param args the command line arguments
-     */
+
     public static void main(String[] args) {
         launch(args);
     }
