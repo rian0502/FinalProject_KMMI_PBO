53d06c9549054ab3787a8499dca37f6e717dd1cb
Miftahul Huda Guntara <90699049+hudalgtr99@users.noreply.github.com>
2021-09-28 09:25:13 +0700

    Update Main.java

--- FinalProjectPBO/src/finalprojectpbo/Main.java
+++ FinalProjectPBO/src/finalprojectpbo/Main.java
@@ -5,26 +5,48 @@
  */
 package finalprojectpbo;
 
+import java.util.ArrayList;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+/**
+ *
+ * @author muham
+ */
 public class Main extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("view/view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 350, 260);
-        stage.setTitle("Hello!");
+        Parent root = FXMLLoader.load(getClass().getResource("view/view.fxml"));
+        
+        Scene scene = new Scene(root);
+        
         stage.setScene(scene);
         stage.show();
     }
-
-
+    
+    /**
+     * @param args the command line arguments
+     */
     public static void main(String[] args) {
         launch(args);
+        
+        ArrayList<Rekening> rekening = new ArrayList<Rekening>();
+            
+        Rekening rek1 = new Rekening(1, 1000000);
+        Rekening rek2 = new Rekening(2, 1500000);
+        Rekening rek3 = new Rekening(3, 2000000);
+
+        rekening.add(rek1);
+        rekening.add(rek2);
+        rekening.add(rek3);
+       
+        
+        Nasabah[] na = {new Individu(11111, 11111, "Nama", "Alamat", rekening), new Perusahaan("NIB", "Nama", "Alamat", rekening)};
+    
     }
     
 }
