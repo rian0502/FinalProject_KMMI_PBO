3d23519c17f0389e0ce6afefd999b4a7a2627434
Miftahul Huda Guntara <90699049+hudalgtr99@users.noreply.github.com>
2021-09-28 08:55:42 +0700

    Update Main.java

--- FinalProjectPBO/src/finalprojectpbo/Main.java
+++ FinalProjectPBO/src/finalprojectpbo/Main.java
@@ -5,6 +5,7 @@
  */
 package finalprojectpbo;
 
+import java.util.ArrayList;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -26,12 +27,26 @@ public class Main extends Application {
         stage.setScene(scene);
         stage.show();
     }
-
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         launch(args);
+        
+        ArrayList<Rekening> rekening = new ArrayList<Rekening>();
+            
+        Rekening rek1 = new Rekening(1, 1000000);
+        Rekening rek2 = new Rekening(2, 1500000);
+        Rekening rek3 = new Rekening(3, 2000000);
+
+        rekening.add(rek1);
+        rekening.add(rek2);
+        rekening.add(rek3);
+       
+        
+        Nasabah[] na = {new Individu(11111, 11111, "Nama", "Alamat", rekening), new Perusahaan("NIB", "Nama", "Alamat", rekening)};
+    
     }
     
 }
